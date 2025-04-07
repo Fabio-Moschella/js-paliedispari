2550ac1b305a572e634ad4f84780a2d2bd623c84
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-07 16:50:27 +0200

    add-pariedispari-track

--- pari e dispari/pari.js
+++ pari e dispari/pari.js
@@ -1,11 +1,39 @@
+// ###############################################################
+// FUNCTIONS
+// ###############################################################
+// ADESSO TRAMITE LA FUNZIONE GENERIAMO UN NUMERODA 1 A 5 PER IL PC..
+function getRandomNumberFrom1To6(max, min) {
+  const randomNumber = Math.floor(Math.random() * max) + min;
+  return randomNumber;
+}
+
+function isFinalSumOddEven() {
+  //se la somma dei numeri è pari
+  if (sumUserAndPcNumber % 2 === 0) {
+    isPari = " Pari";
+    return isPari;
+  } //ALTRIMENTI
+  else {
+    isDispari = "Dispari";
+    // se è dispari
+    return isdispari;
+  }
+}
+// ###############################################################
+// MAIN
+// ###############################################################
+
 // PARI E DISPARI
 // L`UTENTE SCEGLIE TRA PARI E DISPARI...
 const oddAndEven = prompt("scegli pari o dispari");
 // ED INSERISCE UN NUMERO DA 1 A 5
-const userNumber = prompt("inserisci un numero da 1 a 5");
-// ADESSO TRAMITE LA FUNZIONE GENERIAMO UN NUMERODA 1 A 5 PER IL PC..
-function pcRandomNumber() {
-  let pcNumber = Math.floor(Math.random() * 6) + 1;
-  return true;
-}
-console.log(pcRandomNumber(pcNumber));
+const userNumber = prompt("inserisci un numero da 1 a 6");
+// DICHIARO IL VALORE DEL NUMERO DEL PC
+const pcNumber = getRandomNumberFrom1To6(6, 1);
+//STAMPO IL NUMERO DEL PC
+console.log(pcNumber);
+// FACCIO LA SOMMA DEL PC NUMBER E DEL USERNUMBER
+const sumUserAndPcNumber = userNumber + pcNumber;
+// STAMPO LA SOMMA FINALE
+console.log(sumUserAndPcNumber);
+console.log(isFinalSumOddEven());
